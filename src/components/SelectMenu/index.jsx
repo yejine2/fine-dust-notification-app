7cf756c4d0113ead3d5bd0031d2400fbd3ee318a
yejine2 <yejine2@naver.com>
2022-08-21 02:49:07 +0900

    feat: Add Select menu

--- src/components/SelectMenu/index.jsx
+++ src/components/SelectMenu/index.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import {
+  setRegionHandler,
+  fetchData,
+  getAlldustData,
+} from '../../store/dustSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { useLocation } from 'react-router'
+
+import { SIDO_NAME } from '../../utils/constants'
 
 function SelectMenu() {
-  return <div>SelectMenu</div>
+  const dispatch = useDispatch()
+  const location = useLocation().pathname
+  const { dustDataList } = useSelector(getAlldustData)
+
+  const onSidoNameChange = (event) => {
+    if (event.currentTarget.value === '시도') return
+    dispatch(fetchData(event.currentTarget.value))
+  }
+  const onStationNameChange = (event) => {
+    if (event.currentTarget.value === '시군구') return
+    dispatch(setRegionHandler(event.currentTarget.value))
+  }
+
+  return (
+    <>
+      {location !== '/favorite-region' && (
+        <div>
+          <select onChange={onSidoNameChange}>
+            <option>시도</option>
+            {SIDO_NAME.map((sido) => (
+              <option key={sido}>{sido}</option>
+            ))}
+          </select>
+
+          {location !== '/all-region' && (
+            <select onChange={onStationNameChange}>
+              <option>시군구</option>
+              {dustDataList.map((station) => (
+                <option key={station.stationName}>{station.stationName}</option>
+              ))}
+            </select>
+          )}
+        </div>
+      )}
+    </>
+  )
 }
 
 export default SelectMenu
