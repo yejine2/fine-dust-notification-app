b47dcbdde1c75fec91ed8ec0c768f669b542fa97
yejine2 <yejine2@naver.com>
2022-08-20 22:44:23 +0900

    feat: Start Project

---
+++ src/components/SelectMenu/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function SelectMenu() {
+  return <div>SelectMenu</div>
+}
+
+export default SelectMenu
---
+++ src/pages/FavoriteRegion.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function FavoriteRegion() {
+  return <div>FavoriteRegion</div>
+}
+
+export default FavoriteRegion
