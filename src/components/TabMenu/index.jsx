e76dc4682bea720822967d076e73342fa894b10c
yejine2 <yejine2@naver.com>
2022-08-21 01:24:07 +0900

    feat: Add Link in bottom tab menu

--- src/components/TabMenu/index.jsx
+++ src/components/TabMenu/index.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function TabMenu() {
-  return <div>TabMenu</div>
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">내 지역보기</NavLink>
+        </li>
+        <li>
+          <NavLink to="/all-region">전체 시도보기</NavLink>
+        </li>
+        <li>
+          <NavLink to="/favorite-region">즐겨찾기</NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
 }
 
 export default TabMenu
