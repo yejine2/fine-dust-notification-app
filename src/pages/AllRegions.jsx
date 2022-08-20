e26205765d0e90e4ceb93c128acef2c41c5ebc43
yejine2 <yejine2@naver.com>
2022-08-21 01:23:19 +0900

    feat: Add all regions page

--- src/pages/AllRegions.jsx
+++ src/pages/AllRegions.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchData, getAlldustData } from '../store/dustSlice'
+import Card from '../components/Card'
+import SelectMenu from '../components/SelectMenu'
 
 function AllRegions() {
-  return <div>AllRegions</div>
+  const dispatch = useDispatch()
+  const { dustDataList, isLoading } = useSelector(getAlldustData)
+
+  useEffect(() => {
+    dispatch(fetchData('전국'))
+  }, [dispatch])
+
+  return (
+    <div>
+      <h2>전체 지역</h2>
+      <SelectMenu />
+      {isLoading ? (
+        <p>로딩중!</p>
+      ) : (
+        dustDataList.map((item) => <Card key={item.stationName} data={item} />)
+      )}
+    </div>
+  )
 }
 
 export default AllRegions
