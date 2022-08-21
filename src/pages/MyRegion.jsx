6524e7ea8330f9dcbab1552aeb3ae74051022b12
yejine2 <yejine2@naver.com>
2022-08-21 21:01:44 +0900

    feat: Add my region

--- src/pages/MyRegion.jsx
+++ src/pages/MyRegion.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { getAlldustData } from '../store/dustSlice'
+import Card from '../components/Card'
+import SelectMenu from '../components/SelectMenu'
 
 function MyRegion() {
-  return <div>MyRegion</div>
+  const { myRegion } = useSelector(getAlldustData)
+
+  return (
+    <div>
+      <h2>내 지역</h2>
+      <SelectMenu />
+      {myRegion ? <Card data={myRegion} /> : <p>지역을 선택해주세요</p>}
+    </div>
+  )
 }
 
 export default MyRegion
