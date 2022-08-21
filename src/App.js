e926b1e6d5dff4a3b101297e12ee989d9aa7f161
yejine2 <yejine2@naver.com>
2022-08-21 22:07:24 +0900

    feat: Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AllRegions from './pages/AllRegions'
+import FavoriteRegion from './pages/FavoriteRegion'
+import MyRegion from './pages/MyRegion'
+import TabMenu from './components/TabMenu'
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MyRegion />} />
+        <Route path="/all-region" element={<AllRegions />} />
+        <Route path="/favorite-region" element={<FavoriteRegion />} />
+      </Routes>
+      <TabMenu />
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
